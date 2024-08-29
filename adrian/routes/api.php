f37d827809bb2c1d48c7bfa3900c51e8c77a91af
Adrian200727 <adrian.adiputra47@smk.belajar.id>
2024-08-29 09:28:37 +0700

    p

---
+++ adrian/routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\ArticleController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::apiResource('articles', ArticleController::class);
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+    
